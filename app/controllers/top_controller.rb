ed00e5c7f130a227014ab8b2718a58064f8e4218
yanaka-cafe <shinichiro.takabe@gmail.com>
2016-06-21 16:58:17 +0900

    add date

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,8 @@
+require 'rss_get'
+
 class TopController < ApplicationController
   def index
+  	rss_url = 'http://tech.uzabase.com/rss'
+  	@uzabase_rss_summary = RSS::Parser.parse(rss_url)
   end
 end
--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -1,2 +1,10 @@
 module TopHelper
+
+	def title_limit(object)
+		object[0,10]
+	end
+
+	def dscrpt_limit(object)
+		object.gsub(/<p>|<\/p>|<span.+?>|<\/span>|<a.+?>|<\/a>|<img>|<img.+?>|<div.+?>|<\/div>|<br.+?>|<\/br>/, "")[0,30]
+	end
 end
